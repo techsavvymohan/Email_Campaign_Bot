56b2e4b46b4e6a2df6fa072a4349651396fe810a
techsavvymohan <mohansinghdhakrey01@gmail.com>
2026-09-21 01:12:37 +0530

    chore(init): initial Django project configuration and settings

---
+++ email_automation/templatetags/custom_tags.py
@@ -0,0 +1,43 @@
+import os
+import pytz
+from datetime import datetime
+
+from django import template
+from django.utils import timezone
+from django.core import exceptions
+
+register = template.Library()
+
+
+@register.filter
+def filename(value):
+    """
+        returns just the file name
+    """
+    return os.path.basename(value.name)
+
+@register.filter
+def subtract(value, arg):
+    return value - arg
+
+
+@register.simple_tag
+def utc_to_local(utc_datetime, user_timezone, date_format="%b. %d, %Y, %I:%M %p"):
+    
+    if utc_datetime:
+        if not isinstance(utc_datetime, str):
+            utc_datetime = str(utc_datetime)
+
+        try:
+            user_timezone = pytz.timezone(user_timezone)  
+
+        except pytz.UnknownTimeZoneError:
+            pass
+
+        utc_datetime_obj = datetime.fromisoformat(utc_datetime)
+        utc_datetime_obj = utc_datetime_obj.replace(tzinfo=pytz.UTC)
+        
+        specific_datetime = utc_datetime_obj.astimezone(user_timezone)
+        # print("date: ", specific_datetime, specific_datetime.strftime(date_format), date_format)
+        return specific_datetime.strftime(date_format)
+    
---
+++ email_automation/urls.py
@@ -0,0 +1,47 @@
+
+from django.contrib import admin
+from django.conf import settings
+from django.shortcuts import redirect
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+
+from .views import (support_view, rate_limiter_view, view_404, 
+                        handler_403, home_view)
+
+
+admin.site.site_header = 'Email Campaign Studio'           
+admin.site.index_title = 'Campaign Admin'              
+admin.site.site_title = 'Email Campaign Studio Admin'
+admin.site.site_url = "/" 
+
+
+
+handler404 = view_404
+
+handler403 = handler_403
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('support/', support_view, name='support-view'),
+    path('ratelimit-error/', rate_limiter_view, name='ratelimit-error'),
+
+    path("", lambda r: redirect("home")),
+
+    path('free-email-automation-tool/', home_view, name='home'),
+    path('user/', include('user.urls')),
+    path('blog/', include('blog.urls'), name="blogs"),
+    
+    path('email/', include('automail.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
+    
+]
+if settings.DEBUG:
+   urlpatterns +=  []
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+   
+urlpatterns += [ re_path(r'^.*/$', view_404, name='page_not_found'),]
